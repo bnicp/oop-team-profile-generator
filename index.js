6b7641866bd122a00b877108b48864c4c11adcde
bnicp <bnp415@gmail.com>
2022-05-08 19:15:13 -0400

    outline for employee section

---
+++ index.js
@@ -0,0 +1,17 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+function render(teamMembers) {
+    return "this will be html";
+}
+
+function buildTeam() {
+    fs.writeFile("/dist/team.html", render(teamMembers), (err) => {
+        if (err) {
+        console.log(err);
+    }  
+});
+}
