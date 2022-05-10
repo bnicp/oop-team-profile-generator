08c2db5c8edd3aec4334257a8af656098954a7bc
bnicp <bnp415@gmail.com>
2022-05-10 16:53:07 -0400

    added code for each role + video waslkthrough + screenshot + readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# oop-team-profile-generator
+## OOP team profile generator
+
+## Description
+
+This project is a node powered CLI that takes user input to create a website for their team.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Make sure you have node installed in your parent folder to run this app.
+
+## Usage
+
+Type "node index.js" in the terminal and press enter. Answer the questions asked about adding new team members and the app will transmit that information to your html file.
+
+![adding team members.](https://watch.screencastify.com/v/hHwKFPW9nFpIMdQacb8Y)
+
+![rendered html.](./dist/rendered_html.jpg)
+
+## License
+
+MIT License
+
+## Credits
+
+Trey Eckels (Course Instructor) - https://github.com/treyeckels
+
+## Contributing
+
+Clone this code into a new branch and contant me with anything you'd like to commit to main.
+
+## Questions
+
+Email: bnp415@gmail.com
+Github: https://github.com/bnicp
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,32 +1,130 @@
-const Employee = require('../lib/Employee');
+const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    describe:("Intitialization"), () => {
-        it("should instantiate an employee instance", () => {
-            const employee = new Employee()
+  describe("Initialization", () => {
+    it("should instantiate employee instance", () => {
+      
+    // arrange
+      
+      const employee = new Employee();
 
-            expect(typeof employee).tobe("object");
-        });
+    // act
 
-        it("Should set name via constructor arguments", () => {
-            const name = "Alice"
-        })
-     });
-});
+      
+    // assert
 
+      expect(typeof employee).toBe("object");
+    });
 
+    it("should set name via constrcutor argument", () => {
 
+    // arrange
 
+      const name = "Alice";
+    
+    // act
 
-describe("Getter methods", () => {
+      const employee = new Employee(name);
+    
+    // assert
+
+      expect(employee.name).toBe(name);
+    });
+
+    it("should set id via constrcutor argument", () => {
+      
+    // arrange
+
+      const id = 100;
+      
+    // act
+
+      const employee = new Employee("Alice", id);
+      
+    // assert
+
+      expect(employee.id).toBe(id);
+    });
+
+    it("should set email via constrcutor argument", () => {
+      
+    // arrange
+
+      const email = "test@test.com";
+    
+    // act
+
+      const employee = new Employee("Alice", 100, email);
+    
+    // assert
+
+      expect(employee.email).toBe(email);
+    });
+  });
+
+  describe("Getter methods", () => {
     it("should get name via getName()", () => {
+      
+    // arrange
 
-        const name = "Alice";
+      const name = "Alice";
+      
+    // act
 
-        const employee = new Employee(name, 100, "test@test.com")
+      const employee = new Employee(name, 100, "test@test.com");
+      const employeeName = employee.getName();
+      
+    // assert
 
+      expect(employeeName).toBe(name);
+    });
 
+    it("should get id via getId())", () => {
+      
+    // arrange
+      
+        const id = 100;
+      
+    // act
+      
+      const employee = new Employee("alice", id, "test@test.com");
+      const employeeId = employee.getId();
+      
+    // assert
+      
+      expect(employeeId).toBe(id);
+    });
 
+    it("should get email via getEmail()", () => {
+      
+    // arrange
 
-    }
+      const email = "test@test.com";
+      
+    // act
+
+      const employee = new Employee("Alice", 100, email);
+      const employeeEmail = employee.getEmail();
+      
+    // assert
+
+      expect(employeeEmail).toBe(email);
+    });
+
+    it("should get role via getRole()", () => {
+      
+    // arrange
+
+      const role = "Employee";
+      
+    // act
+
+      const employee = new Employee("Alice", 100, "test@test.com");
+      const employeeRole = employee.getRole();
+      
+    // assert
+      
+      expect(employeeRole).toBe(role);
+    });
+  });
 });
